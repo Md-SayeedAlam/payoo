460c87db5d74ddcbb3d3079de10e10a8aa048df2
Md-SayeedAlam <muhammadsayeedalam9@gmail.com>
2024-09-18 20:40:54 +0100

    first commit

---
+++ js/login.js
@@ -0,0 +1,11 @@
+document.getElementById('login-btn').addEventListener('click', function(event){
+ event.preventDefault()
+    
+
+    const phoneNumber = document.getElementById('phone-number').value
+
+    const pinNumber = document.getElementById('pin-number').value
+
+    console.log(phoneNumber,pinNumber)
+})
+
