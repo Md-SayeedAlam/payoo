9aa52119e7b92c9130924ed3ad0e8440b26ae908
Md-SayeedAlam <muhammadsayeedalam9@gmail.com>
2024-09-19 10:51:30 +0100

    added new files

--- js/login.js
+++ js/login.js
@@ -7,7 +7,7 @@ document.getElementById('login-btn').addEventListener('click', function(event){
     const pinNumber = document.getElementById('pin-number').value
 if(phoneNumber === '5' && pinNumber === '1234'){
     console.log('you are logged in')
-    window.location.href='/home.html'
+    window.location.href='./home.html'
 }
 else{
     alert('Wrong phone number or pin')
